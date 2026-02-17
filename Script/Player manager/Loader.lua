b1390d40740d88f397bb95ec1037da147c6843d5
HaZcK <ahmadalkhafidzbadali@gmail.com>
2026-02-17 19:39:33 +0800

    Implement main loader for Player Manager
    Add main loader script for Player Manager functionality.

--- Script/Player manager/Loader.lua
+++ Script/Player manager/Loader.lua
@@ -1,1 +1,14 @@
+-- LOADER UTAMA
+-- Cara dapat link raw: Buka file di GitHub -> Klik tombol "Raw" -> Copy Link-nya
+
+-- 1. Load Main Logic
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HaZcK/ScriptHub/main/Script/Player%20manager/Main.lua"))()
+
+-- 2. Beri jeda sebentar agar UI terbuat
+task.wait(0.2)
+
+-- 3. Load Player List Logic
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HaZcK/ScriptHub/main/Script/Player%20manager/PlayerList.lua"))()
+
+print("Script Player Manager Loaded Successfully!")
 
