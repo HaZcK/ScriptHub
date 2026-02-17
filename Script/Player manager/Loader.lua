694fa4c9cbd1214c5b9842f3264b81ea4e9387c5
HaZcK <ahmadalkhafidzbadali@gmail.com>
2026-02-17 19:43:23 +0800

    Update Loader.lua to use tokenized link for Main.lua
    Updated the link to Main.lua to include a token for access.

--- Script/Player manager/Loader.lua
+++ Script/Player manager/Loader.lua
@@ -2,7 +2,7 @@
 -- Cara dapat link raw: Buka file di GitHub -> Klik tombol "Raw" -> Copy Link-nya
 
 -- 1. Load Main Logic
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HaZcK/ScriptHub/main/Script/Player%20manager/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HaZcK/ScriptHub/refs/heads/main/Script/Player%20manager/Main.lua?token=GHSAT0AAAAAADTPB3T3RBUHRZQJY4QFEIBE2MUKR4A"))()
 
 -- 2. Beri jeda sebentar agar UI terbuat
 task.wait(0.2)
