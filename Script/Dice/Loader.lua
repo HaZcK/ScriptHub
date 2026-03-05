d215256c40881fb9d0ba0b666f3546c4fbc7d24b
HaZcK <ahmadalkhafidzbadali@gmail.com>
2026-03-05 20:56:23 +0800

    Update URL for loading DiceCore.lua

--- Script/Dice/Loader.lua
+++ Script/Dice/Loader.lua
@@ -4,4 +4,4 @@
 -- ╚══════════════════════════════════════════════════╝
 
 -- Ganti dengan raw URL kamu
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HaZcK/ScriptHub/refs/heads/main/Script/Dice/DiceCore.lua?token=GHSAT0AAAAAADTPB3T34GOX4NMDOOU4NYME2NJPPIA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HaZcK/ScriptHub/refs/heads/main/Script/Dice/DiceCore.lua?token=GHSAT0AAAAAADTPB3T3FNCZXRVLEOUOMZEG2NJPWBA"))()
