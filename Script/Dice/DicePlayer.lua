4fa861ddaf350d89d98e3097eb465e1a799a0823
HaZcK <ahmadalkhafidzbadali@gmail.com>
2026-03-06 12:49:47 +0800

    Update and rename DicePlayer.lua.txt to DicePlayer.lua

--- Script/Dice/DicePlayer.lua
+++ Script/Dice/DicePlayer.lua
@@ -11,10 +11,10 @@
 -- ╚══════════════════════════════════════════════════════════════╝
 
 -- ── Load engine dari GitHub ──────────────────────────────────────────
-local RAW_URL_LIBRARY = "RAW_URL_DICELI_BRARY_KAMU"
--- Contoh: "https://raw.githubusercontent.com/NamaKamu/Repo/main/DiceLibrary.lua"
+local RAW_URL_LIBRARY = "https://github.com/HaZcK/ScriptHub/releases/download/Dice/DiceLibrary.lua.txt"
+-- Contoh: "https://github.com/HaZcK/ScriptHub/releases/download/Dice/DiceLibrary.lua.txt"
 
-local Dice = loadstring(game:HttpGet(RAW_URL_LIBRARY))()
+local Dice = loadstring(game:HttpGet(https://github.com/HaZcK/ScriptHub/releases/download/Dice/DiceLibrary.lua.txt))()
 
 -- ── Konfigurasi opsional ─────────────────────────────────────────────
 Dice.SetTitle("🎲  DICE OF FATE")   -- judul window, bisa diganti
