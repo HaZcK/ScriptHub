f2ba8387a6c7b8daea9e5d6c53f25beb026b01b9
HaZcK <ahmadalkhafidzbadali@gmail.com>
2026-03-13 15:48:22 +0800

    Update RAW_PLACELIST URL to use refs/heads

--- SkyMoon/Mainscript.lua
+++ SkyMoon/Mainscript.lua
@@ -1,7 +1,7 @@
 -- 🌙 SkyMoon ScriptHub | Mainscript.lua
 -- by KHAFIDZKTP | github.com/HaZcK/ScriptHub
 
-local RAW_PLACELIST = "https://raw.githubusercontent.com/HaZcK/ScriptHub/main/SkyMoon/PlaceList.json"
+local RAW_PLACELIST = "https://raw.githubusercontent.com/HaZcK/ScriptHub/refs/heads/main/SkyMoon/PlaceList.json"
 
 -- Notification UI
 local function showNotif(msg, color)
