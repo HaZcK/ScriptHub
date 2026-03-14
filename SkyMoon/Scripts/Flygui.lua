b4144bcff26c486011bffa162dce39c5771ee756
HaZcK <ahmadalkhafidzbadali@gmail.com>
2026-03-14 10:14:57 +0800

    Create Flygui.lua

---
+++ SkyMoon/Scripts/Flygui.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
