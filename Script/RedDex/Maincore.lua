5cfdf503e540131ef84a083c3473f22d45ab7616
HaZcK <ahmadalkhafidzbadali@gmail.com>
2026-03-19 11:47:13 +0800

    Update Maincore.lua

--- Script/RedDex/Maincore.lua
+++ Script/RedDex/Maincore.lua
@@ -1,25 +1,18 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
+local player = game.Players.LocalPlayer
+
 local Window = WindUI:CreateWindow({
     Title = "UniverseHub",
-    Icon = "aperture", -- lucide icon
+    Icon = "aperture",
     Author = "Khafidz",
     Folder = "GlobalHub",
-    
-    -- ↓ Optional. You can remove it.
-    --[[ You can set 'rbxassetid://' or video to Background.
-        'rbxassetid://':
-            Background = "rbxassetid://", -- rbxassetid
-        Video:
-            Background = "video:YOUR-RAW-LINK-TO-VIDEO.webm", -- video 
-    --]]
-    
-    -- ↓ Optional. You can remove it.
+        
     User = {
         Enabled = true,
         Anonymous = false,
         Callback = function()
-        print("Display Name: " .. player.DisplayName)
+            print("Display Name: " .. player.DisplayName)
             print("Username: " .. player.Name)
             print("ID: " .. player.UserId)
 
@@ -27,30 +20,27 @@ local Window = WindUI:CreateWindow({
                 Title = "Info Player",
                 Content = "Logged: " .. player.DisplayName,
                 Duration = 5,
-    }),
-  } ,
+            })
+        end,
+    },
 })
 
-  Window:Tag({
+Window:Tag({
     Title = "1.0",
     Icon = "file-text",
     Color = Color3.fromHex("#87CEEB"),
-    Radius = 0.5, -- from 0 to 13
+    Radius = 0.5,
 })
-  
-  local Updatelog = Window:Tab({
+
+local Updatelog = Window:Tab({
     Title = "Update Log",
-    Icon = "scroll-text", -- optional
+    Icon = "scroll-text",
     Locked = false,
 })
-      
-Tab:Select()
-  
+
 local Paragraph = Updatelog:Paragraph({
     Title = "Update_Log",
     Desc = "WHERE IS THERE AN UPDATE THAT IS STILL RELEASED",
     Color = "Blue",
     Locked = false
 })
-
-    
